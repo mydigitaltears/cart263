06692beadb1a1f4a6026e639219752f53ed48792
xavier <xavier.touikan@gmail.com>
2019-01-29 12:33:10 -0500

    Project1: working on stuff

--- project1/js/script.js
+++ project1/js/script.js
@@ -7,8 +7,19 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let counter = 0;
+let topOfTheHill = 1000000;
 
 
 $( document ).ready(function() {
     console.log( "ready!" );
+    setInterval(update,1000);
 });
+
+function update() {
+  document.getElementById('text').innerHTML = counter;
+  counter++;
+  if (counter == 10){
+    counter = 0;
+  }
+}
