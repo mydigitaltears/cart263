66fb968143d78e710a9a6ca52de06dcea329cb70
xavier <xavier.touikan@gmail.com>
2019-01-29 11:26:47 -0500

    Project1: started project 1

---
+++ project1/js/script.js
@@ -0,0 +1,14 @@
+/*****************
+
+Project 1
+Xavier Touikan
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
