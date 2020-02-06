92cf99c942eb0100d7cffae5b0bd49a918f9cb2b
Xavier <xavier.touikan@gmail.com>
2020-02-06 17:35:28 -0500

    Project1: implemented different travel ads depending on form answers

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -25,6 +25,9 @@ let firstName;
 let lastName;
 let favoriteFood;
 
+//intervals
+let startInterval;
+
 function setup() {
   // Associating all of my variables
   vid = $("#myVideo");
@@ -37,10 +40,20 @@ function setup() {
   p1 = $("#p1");
   p2 = $("#p2");
   travelAd = $(".travelAd");
+  startInterval = setInterval(backgroundFlash, 100);
+}
+
+function backgroundFlash() {
+  let r = Math.random()*255;
+  let g = Math.random()*255;
+  let b = Math.random()*255;
+  $("body").css("background-color", `rgb(${r},${g},${b})`)
 }
 
 function startButtonPressed() {
   //alert('Please fill out this form!');
+  clearInterval(startInterval);
+  $("body").css("background-color", `white`)
   start.css("display", "none");
   firstForm.css("display", "block");
 }
@@ -88,24 +101,36 @@ function libanese() {
   console.log("libanese");
   $(".travelAd h1").text(`${firstName}, Lebanon awaits you!`);
   $(".travelAd p").text("Click here for more informations");
+  $("body").css("background-image",'url("https://res.cloudinary.com/sagacity/image/upload/c_crop,h_2667,w_4000,x_0,y_0/c_limit,dpr_3.0,f_auto,fl_lossy,q_80,w_500/shutterstock_600019766_yjcu5f.jpg")');
+  $(".travelAd").css("background-color", "background-color: rgba(20,140,255,0.8)");
+  $(".travelAd").css("text-shadow", "1px 1px orange");
 }
 
 function italian() {
   console.log("italian");
   $(".travelAd h1").text(`${firstName}, Italy awaits you!`);
   $(".travelAd p").text("Click here for more informations");
+  $("body").css("background-image",'url("https://indietravelpodcast.com/wp-content/uploads/2016/04/Dollarphotoclub_italy-1920x1280.jpg")');
+  $(".travelAd").css("background-color", "background-color: rgba(0,200,80,0.8)");
+  $(".travelAd").css("text-shadow", "1px 1px red");
 }
 
 function indian() {
   console.log("indian");
   $(".travelAd h1").text(`${firstName}, India awaits you!`);
   $(".travelAd p").text("Click here for more informations");
+  $("body").css("background-image",'url("https://cdn.britannica.com/86/170586-050-AB7FEFAE/Taj-Mahal-Agra-India.jpg")');
+  $(".travelAd").css("background-color", "background-color: rgba(200,140,255,0.8)");
+  $(".travelAd").css("text-shadow", "2px 2px orange");
 }
 
 function mexican() {
   console.log("mexican");
   $(".travelAd h1").text(`${firstName}, Mexico awaits you!`);
   $(".travelAd p").text("Click here for more informations");
+  $("body").css("background-image",'url("https://cdn.britannica.com/60/92960-050-327CF926/pyramid-Mayan-Chichen-Itza-Mex.jpg")');
+  $(".travelAd").css("background-color", "background-color: rgba(255,100,40,0.8)");
+  $(".travelAd").css("text-shadow", "2px 2px green");
 }
 
 function travelButtonPressed() {
